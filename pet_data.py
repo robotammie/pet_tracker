570bc8897b8af1ba94510318837752ad145643e5
Tammie Kahnhauser <tammie.kahnhauser@gmail.com>
2025-05-17 18:09:02 -0700

    add age logic

--- pet_data.py
+++ pet_data.py
@@ -1,10 +1,30 @@
 import os
+from datetime import datetime
+from dateutil import relativedelta
+from pytz import timezone
 from . import model
 
 def all():
-  pets = model.Pet.query.all()
+  pet_data = model.Pet.query.all()
+
+  pets = []
+  for pet in pet_data:
+     pets.append(
+        {
+           'name': pet.name,
+           'age': age(pet.birthdate)
+        }
+     )
 
   return(pets)
+
+def age(birthdate):
+  delta = relativedelta.relativedelta(datetime.now(timezone('America/Los_Angeles')), birthdate)
+  if delta.years <= 2:
+    return('{} months'.format(delta.months + (delta.years * 12)))
+
+  return('{} years'.format(delta.years))
+
 
 if __name__ == '__main__':
     from server import app
@@ -12,4 +32,3 @@ if __name__ == '__main__':
     with app.app_context():
       model.connect_to_db(app, os.environ.get('DATABASE_URL'))
       print('Connected')
-      print(pet_data())
